7f633474d73a71a8853ecfdc088b6a251b377d95
Chris Sadler <sadlercr@rose-hulman.edu>
2016-03-26 17:13:22 +0100

    added a abstract Card class.

---
+++ StudyCard/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public abstract class Card
+{
+    int _id;
+    string _frontText;
+    string _backText;
+    public string FrontText
+    {
+        get
+        { return this._frontText; }
+    }
+    public string BackText
+    { get { return this._backText; } }
+}
